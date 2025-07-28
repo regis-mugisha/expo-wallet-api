e4e410930863bcc6007835552de28b9f4bb2c1c5
regis-mugisha <mugisharegis72@gmail.com>
2025-07-28 18:34:45 +0200

    initialize neon database

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import "dotenv/config";
+import { sql } from "./config/db.js";
+import ratelimiter from "./middleware/rateLimiter.js";
+import transactionsRoute from "./routes/transactionsRoute.js";
+
+const app = express();
+app.use(ratelimiter);
+// middleware to parse JSON bodies
+app.use(express.json());
+const PORT = process.env.PORT || 5001;
+
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+    id SERIAL PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialized successfully.");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    process.exit(1);
+  }
+}
+
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
